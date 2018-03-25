c5ff043450b71b003e36963d63b4e653788d255e
itechj <itechjavalab@gmail.com>
2018-03-25 21:43:08 +0300

    Add e2e tests for Todos

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -5,10 +5,49 @@ describe('todo-app App', () => {
 
   beforeEach(() => {
     page = new TodoAppPage();
+    page.navigateTo();
   });
 
-  it('should display message saying app works', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+  it('should display "Todos" header', async () => {
+    expect(await page.getAppHeader()).toEqual('Todos');
+  });
+
+  describe('todos', () => {
+    beforeEach(() => {
+      // Simulate test data
+      page.addTodo('Test TODO #1');
+      page.addTodo('Test TODO #2');
+      page.changeTodoCompletion('Test TODO #2');
+      page.addTodo('Test TODO #3');
+    });
+
+    it('should add a todo', async () => {
+      const todosCount = await page.getTodos().count();
+
+      const testTodo = 'New Test TODO';
+      page.addTodo(testTodo);
+
+      const resultingTodos = page.getTodos();
+      expect(await resultingTodos.count()).toBe(todosCount + 1);
+      expect(await resultingTodos.last().getText()).toBe(testTodo);
+    });
+
+    it('should complete a todo', async () => {
+      const testTodo = 'New Test TODO';
+      page.addTodo(testTodo);
+
+      page.changeTodoCompletion(testTodo);
+
+      expect(await page.isTodoCompeted(testTodo)).toBe(true);
+    });
+
+    it('should delete a todo', async () => {
+      const testTodo = 'New Test TODO';
+      page.addTodo(testTodo);
+
+      page.deleteTodo(testTodo);
+
+      expect(await page.findTodo(testTodo).isPresent()).toBe(false);
+    });
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,53 @@
-import { browser, element, by } from 'protractor';
+import { browser, element, by, Key } from 'protractor';
 
 export class TodoAppPage {
+  todoSelector = '.todo-list li';
+  completeToggleSelector = '.toggle';
+  deleteBtnSelector = '.destroy';
+
   navigateTo() {
     return browser.get('/');
   }
 
-  getParagraphText() {
+  getAppHeader() {
     return element(by.css('app-root h1')).getText();
+  }
+
+  getNewTodoInput() {
+    return element(by.css('input.new-todo'));
+  }
+
+  addTodo(text: string) {
+    this.getNewTodoInput().sendKeys(text, Key.ENTER);
+  }
+
+  getTodos() {
+    return element.all(by.css(this.todoSelector));
+  }
+
+  findTodo(text: string) {
+    return element(by.cssContainingText(this.todoSelector, text));
+  }
+
+  getCompletionToggle(text: string) {
+    return this.findTodo(text).element(by.css(this.completeToggleSelector));
+  }
+
+  changeTodoCompletion(text: string) {
+    this.getCompletionToggle(text).click();
+  }
+
+  deleteTodo(text: string) {
+    const todo = this.findTodo(text);
+    this.hoverElement(todo); // In order to make delete button visible
+    todo.element(by.css(this.deleteBtnSelector)).click();
+  }
+
+  isTodoCompeted(text: string) {
+    return this.getCompletionToggle(text).isSelected();
+  }
+
+  hoverElement(elem) {
+    browser.actions().mouseMove(elem).perform();
   }
 }
